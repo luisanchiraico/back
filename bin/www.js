3ee6689aa754d3dfdb2f415856991c0913772c98
deepMerge <arodriguezv22@gmail.com>
2020-04-27 19:44:36 -0500

    fix server listen

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,12 @@
 
 const app = require('../app');
 const config = require('./config');
+var http = require('http');
+var port = (config.environment.PORT);
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
 
-app.listen(config.environment.PORT, () => {
-    console.log(`API REST corriendo en http://localhost:${config.environment.PORT}`);
-});
+// app.listen(config.environment.PORT, () => {
+//     console.log(`API REST corriendo en http://localhost:${config.environment.PORT}`);
+// });
